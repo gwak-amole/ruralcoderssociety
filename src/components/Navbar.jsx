7605a037a171d6a42ac0a66cacda023143226746
gwak-amole <gwaksophie@gmail.com>
2026-07-10 21:27:58 -0700

    Add hero
    Will make the hero of the website a photo gallery scroller soon, this is a placeholder

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,12 +1,21 @@
 import { Link } from 'react-router-dom'
+import logo from '../assets/logo.png';
 
 const Navbar = () => {
     return(
-        <nav>
-            <Link to="/" style={{}}>Home</Link>
-            <Link to="/about" style={{}}>About</Link>
-            <Link to="/contact" style={{}}>Contact</Link>
+        <div className="flex w-full items-center bg-blue-950 pt-4">
+            <div className="flex justify-center p-4 absolute left-6">
+                <img src={logo} alt="Logo" className="h-18 w-auto"></img>
+            </div>
+            <nav className="mx-auto bg-blue-950 p-4 text-center text-lg font-sans text-white -mt-2">
+            <Link to="/" className="p-2 transition-colors duration-100 hover:text-yellow-400">Home</Link>
+            <Link to="/programs" className="p-2 transition-colors duration-100 hover:text-yellow-400">Programs</Link>
+            <Link to="/about" className="p-2 transition-colors duration-100 hover:text-yellow-400">About</Link>
+            <Link to="/join" className="p-2 transition-colors duration-100 hover:text-yellow-400">Join Us</Link>
+            <Link to="/donate" className="p-2 text-yellow-300 font-semibold border-2 border-yellow-300 rounded-lg m-1 transition-colors duration-100 hover:text-yellow-500 hover:border-yellow-500">Donate</Link>
         </nav>
+        </div>
+        
     );
 }
 
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,15 +1,9 @@
-import {BrowserRouter as Router, Route} from 'react-router-dom'
-
-import MyButton from '../components/Button'
-import Navbar from '../components/Navbar';
-
-function App() {
+function Home() {
   return (
-    <div className="App">
-        <h1>My React Website</h1>
-        <MyButton />
+    <div className="h-128 w-full bg-linear-to-b from-blue-950 from-65% via-blue-950 via-70% to-white">
+        <h1 className="font-title text-7xl text-center p-32 pt-45 font-semibold text-white">Coding for <span className="text-yellow-300">Every Zip Code</span></h1>
     </div>
   )
 }
 
-export default App;
+export default Home;
