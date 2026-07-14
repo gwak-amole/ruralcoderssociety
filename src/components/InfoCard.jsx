94ae8b601514eaf7c4920fcdb14935a610169a7f
gwak-amole <gwaksophie@gmail.com>
2026-07-14 14:57:32 -0600

    Update InfoCard styling

--- src/components/InfoCard.jsx
+++ src/components/InfoCard.jsx
@@ -1,9 +1,9 @@
 
 const InfoCard = ({ number = "", desc = ""}) => {
     return(
-        <div className="border-8 border-blue-900 p-5 m-3 w-100 rounded-3xl bg-blue-950">
-            <h1 className="text-4xl font-semibold m-5 text-amber-300">{number}</h1>
-            <p className="m-3">{desc}</p>
+        <div className="border-0 p-8 m-3 w-100 rounded-3xl bg-blue-400">
+            <h1 className="text-6xl font-semibold m-5 text-amber-300">{number}</h1>
+            <p className="m-3 text-xl text-amber-400">{desc}</p>
         </div>
     );
 }
