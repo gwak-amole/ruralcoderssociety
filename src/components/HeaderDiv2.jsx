893715b9cf9e411d6fc55ddf3196dc746d43afad
gwak-amole <gwaksophie@gmail.com>
2026-07-14 19:45:32 -0600

    Add more HeaderDiv padding

--- src/components/HeaderDiv2.jsx
+++ src/components/HeaderDiv2.jsx
@@ -1,6 +1,6 @@
 const HeaderDiv2 = ({header="Header", content}) => {
     return(
-        <div className="border-2 rounded-2xl m-5 py-5 bg-blue-950 text-white">
+        <div className="border-2 rounded-2xl m-5 py-10 bg-blue-950 text-white">
             <h1 className="text-6xl m-5 mt-8 font-semibold text-white">{header}</h1>
             {content}
         </div>
