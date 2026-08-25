a309f802cd2163494b6e7a394bc1de47440f2d90
gwak-amole <gwaksophie@gmail.com>
2026-08-25 13:51:26 -0600

    Add image styling

--- src/components/InfoCard2.jsx
+++ src/components/InfoCard2.jsx
@@ -3,7 +3,7 @@ const InfoCard2 = ({ header = "", desc = "", image_link }) => {
         <div className="border-2 p-8 m-2 w-96 rounded-3xl bg-blue-100 border-amber-800 hover:-translate-y-0.5 hover:bg-white duration-150 ease-in-out">
             <h1 className="text-4xl font-semibold m-5 text-blue-950">{header}</h1>
             <p className="m-3 text-xl text-blue-950">{desc}</p>
-            <img src={image_link}></img>
+            <img className="border-amber-300 border-2 rounded-xl" src={image_link}></img>
         </div>
     );
 }
